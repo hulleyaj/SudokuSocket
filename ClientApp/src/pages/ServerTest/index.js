102143d1b1f2c345e02813c776d6bc4713aafe8e
hulleyaj <46404338+hulleyaj@users.noreply.github.com>
2020-06-04 13:02:21 -0400

    Eslint fix, pipeline fix

--- ClientApp/src/pages/ServerTest/index.js
+++ ClientApp/src/pages/ServerTest/index.js
@@ -11,6 +11,7 @@ const ServerTest = observer(() => {
     status
   } = serverTest;
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => reset, []);
 
   return <React.Fragment>

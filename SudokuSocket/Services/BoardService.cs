534f0c4a1466149414d59e933ab9a7453425fca7
hulleyaj <46404338+hulleyaj@users.noreply.github.com>
2020-07-19 19:43:32 -0400

    Creates board with bad return statement

--- SudokuSocket/Services/BoardService.cs
+++ SudokuSocket/Services/BoardService.cs
@@ -1,15 +1,204 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SudokuSocket.Services
 {
+    //https://www.geeksforgeeks.org/program-sudoku-generator/
+    //Following is the improved logic for the problem.
+    //1. Fill all the diagonal 3x3 matrices.
+    //2. Fill recursively rest of the non-diagonal matrices.
+    //   For every cell to be filled, we try all numbers until
+    //   we find a safe number to be placed.
+    //3. Once matrix is fully filled, remove K elements
+    //   randomly to complete game.
+
+    // [row][col]
+    //"Square Index" is as follows
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
     public class BoardService
     {
-        public static int CreateBoard()
+        private static Random rGen = new Random();
+
+        public static HashSet<Tuple<int, int>> RESTRICTED_INDEXES = Enumerable.Range(0, 9).Select(f => new Tuple<int, int>(f, (f / 3) * 3)).ToHashSet();
+
+        public static List<List<int>> CreateBoard()
         {
-            return 1;
+            List<List<int>> board = InitializeEmptyBoard();
+            GenerateDiagonalBigSquares(board);
+            FillBoardRecursively(board, 0, 3);
+
+            return board;
         }
+
+        public static List<List<int>> InitializeEmptyBoard() => Enumerable.Range(0, 9).Select(f => Enumerable.Repeat(0, 9).ToList()).ToList();
+
+        public static void GenerateDiagonalBigSquares(List<List<int>> board)
+        {
+            for (int square = 0; square <= 2; square++)
+            {
+                int row = square * 3;
+                int startingCol = row;
+                int col = row;
+                List<int> availableNumbers = Enumerable.Range(1, 9).ToList();
+
+                for (int i = 9; i > 0; i--)
+                {
+                    int index = rGen.Next(0, i);
+
+                    board[row][col] = availableNumbers[index];
+
+                    availableNumbers.RemoveAt(index);
+
+                    if (i % 3 == 1)
+                    {
+                        row++;
+                        col = startingCol;
+                    }
+                    else
+                    {
+                        col++;
+                    }
+                }
+            }
+        }
+
+        public static void FillBoardRecursively(List<List<int>> board, int row, int col)
+        {
+            List<int> remainingNumbers = GetAvailableNumbers(board, row, col).OrderBy(f => rGen.Next()).ToList();
+
+            foreach (int number in remainingNumbers)
+            {
+                board[row][col] = number;
+                DebuggerPrintBoard(board);
+                if (row == 8 && col == 5)
+                {
+                    return;
+                }
+
+                if (col == 8)
+                {
+                    if (RESTRICTED_INDEXES.Contains(new Tuple<int, int>(row + 1, 0)))
+                    {
+                        FillBoardRecursively(board, row + 1, 3);
+                    }
+                    else
+                    {
+                        FillBoardRecursively(board, row + 1, 0);
+                    }   
+                }
+                else if (RESTRICTED_INDEXES.Contains(new Tuple<int, int>(row, col + 1)))
+                {
+                    if (col + 4 >= 9)
+                    {
+                        FillBoardRecursively(board, row + 1, 0);
+                    }
+                    else
+                    {
+                        FillBoardRecursively(board, row, col + 4);
+                    }
+                }
+                else
+                {
+                    FillBoardRecursively(board, row, col + 1);
+                }
+            }
+
+            board[row][col] = 0;
+        }
+
+        #region AvailableNumbers
+
+        public static IEnumerable<int> GetAvailableNumbers(List<List<int>> board, int row, int col)
+        {
+            return Enumerable.Range(1, 9)
+                .Except(board[row])
+                .Except(board.Select(f => f[col]))
+                .Except(GetUsedNumbersBySquare(board, row, col));
+        }
+
+        public static IEnumerable<int> GetUsedNumbersBySquare(List<List<int>> board, int row, int col)
+        {
+            row = (row / 3) * 3;
+            col = (col / 3) * 3;
+
+            List<int> numbers = new List<int>();
+
+            for (int i = row; i <= row + 2; i++)
+            {
+                for (int j = col; j <= col + 2; j++)
+                {
+                    numbers.Add(board[i][j]);
+                }
+            }
+
+            return numbers;
+        }
+
+        #endregion AvailableNumbers
+
+        #region BoardValidation
+        public static bool SquareCheck(List<List<int>> board, int squareIndex)
+        {
+            int row = (squareIndex / 3) * 3;
+            int col = (squareIndex % 3) * 3;
+
+            List<int> numbers = new List<int>();
+
+            for (int i = row; i <= row + 2; i++)
+            {
+                for (int j = col; j <= col + 2; j++)
+                {
+                    numbers.Add(board[i][j]);
+                }
+            }
+
+            return numbers.Distinct().Except(new List<int>() { 0 }).Count() == 9;
+        }
+
+        public static bool RowCheck(List<List<int>> board, int index) => board[index].Distinct().Count() == board[index].Count();
+
+        public static bool ColumnCheck(List<List<int>> board, int index) => board.Select(f => f[index]).Distinct().Count() == board.Select(f => f[index]).Count();
+
+        #endregion BoardValidation
+
+        #region PrintBoard
+        public static void DebuggerPrintBoard(List<List<int>> board)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int rowIndex = 0; rowIndex <= 8; rowIndex++)
+            {
+                for (int colIndex = 0; colIndex <= 8; colIndex++)
+                {
+                    sb.Append(board[rowIndex][colIndex]);
+
+                    if ((colIndex + 1) % 3 == 0)
+                    {
+                        sb.Append("   ");
+                    }
+                }
+
+                sb.AppendLine();
+
+                if ((rowIndex + 1) % 3 == 0)
+                {
+                    sb.AppendLine();
+                }
+            }
+
+            sb.AppendLine("----------------------------------");
+
+            Console.WriteLine(sb.ToString());
+            Debug.WriteLine(sb.ToString());
+        }
+
+        #endregion PrintBoard
     }
 }
--- SudokuSocketTests/BoardServiceTest.cs
+++ SudokuSocketTests/BoardServiceTest.cs
@@ -1,20 +1,190 @@
 using NUnit.Framework;
 using SudokuSocket.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SudokuSocket.Tests
 {
     public class Tests
     {
+        private List<List<int>> solvedBoard = new List<List<int>>()
+        {
+            new List<int>() { 8, 2, 7, 1, 5, 4, 3, 9, 6 },
+            new List<int>() { 9, 6, 5, 3, 2, 7, 1, 4, 8 },
+            new List<int>() { 3, 4, 1, 6, 8, 9, 7, 5, 2 },
+            new List<int>() { 5, 9, 3, 4, 6, 8, 2, 7, 1 },
+            new List<int>() { 4, 7, 2, 5, 1, 3, 6, 8, 9 },
+            new List<int>() { 6, 1, 8, 9, 7, 2, 4, 3, 5 },
+            new List<int>() { 7, 8, 6, 2, 3, 5, 9, 1, 4 },
+            new List<int>() { 1, 5, 4, 7, 9, 6, 8, 2, 3 },
+            new List<int>() { 2, 3, 9, 8, 4, 1, 5, 6, 7 }
+        };
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Test1()
+        public void Should_InitializeEmptyBoard()
         {
-            int retVal = BoardService.CreateBoard();
-            Assert.AreEqual(1, retVal);
+            List<List<int>> board = BoardService.InitializeEmptyBoard();
+
+            for (int i = 0; i <= 8; i++)
+            {
+                for (int j = 0; j <= 8; j++)
+                {
+                    Assert.AreEqual(0, board[i][j]);
+                }
+            }
+        }
+
+        [Test]
+        public void Should_have_own_reference_lists_on_board()
+        {
+            List<List<int>> board = BoardService.InitializeEmptyBoard();
+
+            board[0][0] = 5;
+
+            Assert.AreEqual(0, board[1][0]);
+        }
+
+        [Test]
+        public void Should_return_true_SquareCheck()
+        {
+            Assert.AreEqual(true, BoardService.SquareCheck(solvedBoard, 4));
+        }
+
+        [Test]
+        public void Should_return_false_SquareCheck()
+        {
+            List<List<int>> badBoard = solvedBoard.Select(f => new List<int>(f)).ToList();
+            badBoard[3][3] = 6;
+
+            Assert.AreEqual(false, BoardService.SquareCheck(badBoard, 4));
+        }
+
+        [Test]
+        public void Should_return_true_RowCheck()
+        {
+            List<List<int>> board = new List<List<int>>() { new List<int>() { 8, 2, 7, 1, 5, 4, 3, 9, 6 } };
+
+            Assert.AreEqual(true, BoardService.RowCheck(board, 0));
+        }
+
+        [Test]
+        public void Should_return_false_RowCheck()
+        {
+            List<List<int>> board = new List<List<int>>() { new List<int>() { 2, 2, 7, 1, 5, 4, 3, 9, 6 } };
+
+            Assert.AreEqual(false, BoardService.RowCheck(board, 0));
+        }
+
+        [Test]
+        public void Should_return_true_ColumnCheck()
+        {
+            List<List<int>> board = new List<List<int>>() 
+            {
+                new List<int>() { 8 },
+                new List<int>() { 9 },
+                new List<int>() { 3 },
+                new List<int>() { 5 },
+                new List<int>() { 4 },
+                new List<int>() { 6 },
+                new List<int>() { 7 },
+                new List<int>() { 1 },
+                new List<int>() { 2 }
+            };
+
+            Assert.AreEqual(true, BoardService.ColumnCheck(board, 0));
+        }
+
+        [Test]
+        public void Should_return_false_ColumnCheck()
+        {
+            List<List<int>> board = new List<List<int>>() 
+            {
+                new List<int>() { 9 },
+                new List<int>() { 9 },
+                new List<int>() { 3 },
+                new List<int>() { 5 },
+                new List<int>() { 4 },
+                new List<int>() { 6 },
+                new List<int>() { 7 },
+                new List<int>() { 1 },
+                new List<int>() { 2 }
+            };
+
+            Assert.AreEqual(false, BoardService.ColumnCheck(board, 0));
+        }
+
+        [Test]
+        public void Should_GenerateDiagonalBigSquares()
+        {
+            List<List<int>> board = BoardService.InitializeEmptyBoard();
+            BoardService.GenerateDiagonalBigSquares(board);
+
+            for (int i = 0; i <= 8; i++)
+            {
+                Assert.AreEqual(3, board[i].Except(new List<int>() { 0 }).ToList().Count);
+            }
+
+            Assert.AreEqual(true, BoardService.SquareCheck(board, 0));
+            Assert.AreEqual(true, BoardService.SquareCheck(board, 4));
+            Assert.AreEqual(true, BoardService.SquareCheck(board, 8));
+        }
+
+        [Test]
+        public void Should_GetUsedNumbersBySquare()
+        {
+            List<List<int>> board = new List<List<int>>
+            {
+                new List<int>() { 8, 0, 7 },
+                new List<int>() { 0, 6, 5 },
+                new List<int>() { 3, 4, 0 },
+            };
+
+            IEnumerable<int> retVal = BoardService.GetUsedNumbersBySquare(board, 2, 2);
+
+            Assert.AreEqual(true, !retVal.Except(new List<int>() { 2, 9, 1 }).Any());
+        }
+
+        [Test]
+        public void Should_GetAvailableNumbers()
+        {
+            List<List<int>> board = new List<List<int>>
+            {
+                new List<int>() { 8, 2, 7, 1, 5, 4, 3, 9, 6 },
+                new List<int>() { 9, 6, 5, 3, 2, 7, 1, 4, 8 },
+                new List<int>() { 3, 4, 1, 6, 8, 9, 7, 5, 2 },
+                new List<int>() { 5, 9, 3, 0, 6, 8, 2, 7, 1 },
+                new List<int>() { 0, 7, 2, 5, 0, 3, 6, 8, 0 },
+                new List<int>() { 6, 1, 8, 0, 7, 2, 4, 3, 5 },
+                new List<int>() { 7, 8, 6, 2, 3, 5, 9, 1, 4 },
+                new List<int>() { 1, 5, 4, 7, 0, 6, 8, 2, 3 },
+                new List<int>() { 2, 3, 9, 8, 0, 1, 5, 6, 7 }
+            };
+
+            IEnumerable<int> retVal = BoardService.GetAvailableNumbers(board, 4, 4);
+
+            Assert.AreEqual(true, !retVal.Except(new List<int>() { 9, 4, 1 }).Any());
+        }
+
+        [Test]
+        public void Should_GenerateRestrictedIndexes()
+        {
+            Assert.AreEqual(true, BoardService.RESTRICTED_INDEXES.Contains(new Tuple<int, int>(0, 0)));
+            Assert.AreEqual(false, BoardService.RESTRICTED_INDEXES.Contains(new Tuple<int, int>(0, 1)));
+            Assert.AreEqual(9, BoardService.RESTRICTED_INDEXES.Count);
+        }
+
+        [Test]
+        public void Should_CreateBoard()
+        {
+            List<List<int>> retVal = BoardService.CreateBoard();
+
+            int x = 2;
         }
     }
 }
